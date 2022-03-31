a63026e60579d5205a57ae8a29b153a697b439b7
Arseniy Boltzmunn <ars.boltz@gmail.com>
2022-04-01 01:08:46 +0300

    boardTasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import {useState} from "react";
 
 const App = () => {
-  return (
-      <div>
+    const [board, setBoard] = useState([
+        {id: 1, title: 'Todo', items: [{id:1, title: 'visit to market'},{id:2, title: 'throw out rhe trash'}, {id: 3, title: 'Eat'}]},
+        {id: 1, title: 'Check', items: [{id:1, title: 'REACT KOD'},{id:2, title: 'REACT TASKS'}, {id: 3, title: 'EAT'} ]},
+        {id: 1, title: 'Done', items: [{id:1, title: 'Go to gym'},{id:2, title: 'Cleaning in the room'}, {id: 3, title: 'Eat'} ]}
+    ])
 
+
+  return (
+      <div className='App'>
+          {board.map(board =>
+          <div className='board'>
+              <div className='board__title'>
+              {board.title}
+
+              </div>
+          {board.items.map(item =>
+              <div className='item'>
+                  {item.title}
+
+              </div>
+              )}
+          </div>
+          )}
       </div>
   )
 }
